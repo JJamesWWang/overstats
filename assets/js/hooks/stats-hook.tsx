e07626d8ebc97aeddeb1a6f700af64acb093b3f4
James S. Wang <jjameswwang@gmail.com>
2022-12-20 21:48:15 -0500

    Stats: Set up re-rendering with React

--- assets/js/hooks/stats-hook.tsx
+++ assets/js/hooks/stats-hook.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const StatsPage = () => {};
+const StatsPage = (props) => {
+  return <P>{props.data ? props.data : "None"}</P>;
+};
+
+const P = (props) => {
+  return (
+    <p className="leading-5 text-gray-600 dark:text-gray-400 mb-2">{props.children}</p>
+  );
+};
 
 const StatsHook = {
   mounted() {
-    let data = this.el.dataset;
-    console.log(data);
+    let statsData;
 
     const element = document.getElementById("stats-hook");
     const root = createRoot(element);
-    root.render(<StatsPage data={data} />);
+    root.render(<StatsPage data={statsData} />);
+
+    this.handleEvent("stats_data_updated", ({ data }) => {
+      statsData = data;
+      console.log(statsData);
+      root.render(<StatsPage data={statsData.name} />);
+    });
   },
 };
 
--- lib/overstats_web/live/pages/stats_live.ex
+++ lib/overstats_web/live/pages/stats_live.ex
@@ -1,15 +1,24 @@
 defmodule OverstatsWeb.StatsLive do
   use OverstatsWeb, :live_view
   alias OverstatsWeb.Header
+  alias Overstats.Players
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign(all_players: Players.list_players())
+     |> assign(player: [])}
   end
 
   @impl true
-  def handle_params(params, _uri, socket) do
-    {:noreply, socket}
+  def handle_event(
+        "stats_data_submit",
+        %{"stats_data" => %{"player_name" => player_name}},
+        socket
+      ) do
+    player = Players.get_player_by_name!(player_name)
+    {:noreply, socket |> push_event("stats_data_updated", %{data: %{name: player.name}})}
   end
 
   @impl true
@@ -18,7 +27,28 @@ defmodule OverstatsWeb.StatsLive do
     <Header.render page="stats" />
     <.container max_width="lg">
       <.h2>Stats</.h2>
-      <div id="stats-hook" phx-hook="StatsHook" data-stats={["hello"]} />
+      <.form
+        :let={f}
+        for={:stats_data}
+        phx-submit="stats_data_submit"
+        class="flex flex-col gap-y-3 items-start"
+      >
+        <.h3>Choose a player</.h3>
+
+        <div>
+          <.form_label form={f} field={:player_name} />
+          <.select
+            options={@all_players |> Enum.map(&{&1.name, &1.name})}
+            form={f}
+            field={:player_name}
+          />
+          <.form_field_error form={f} field={:player_name} />
+        </div>
+
+        <.button class="submit" label="Continue" />
+      </.form>
+
+      <div id="stats-hook" phx-hook="StatsHook" />
     </.container>
     """
   end
